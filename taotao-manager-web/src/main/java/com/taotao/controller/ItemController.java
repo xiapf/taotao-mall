c718f868feb9c1cd7972fb03dd6b520a08f6d89d
xiapf <xiapengfei_nuist@163.com>
2018-07-15 23:01:57 +0800

    挪目录

--- taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.taotao.controller;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @Date: 2018/7/15 19:33
  * @Version: 1.0
  */
+@Controller
 public class ItemController {
 
     @Autowired
